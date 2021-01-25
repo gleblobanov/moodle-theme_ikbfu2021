046d15f9a391efa3a8a23148415f1075ef7e2a9e
Gleb Lobanov <gleblobanov@users.noreply.github.com>
2021-01-25 11:47:37 +0200

    Initial

---
+++ version.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+* @package   theme_ikbfu
+* @copyright 2021 Gleb Lobanov
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = '2021012500';
+
+$plugin->requires = '2020061500';
+
+$plugin->component = 'theme_ikbfu';
+
+$plugin->dependencies = [
+    'theme_boost' => '2020061500'
+];
+
