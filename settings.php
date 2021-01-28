bf46868b366dafce07cc0b76fc7981195a0a598f
Gleb Lobanov <gleblobanov@users.noreply.github.com>
2021-01-28 17:18:15 +0200

    Fix name of the component.

--- settings.php
+++ settings.php
@@ -14,22 +14,22 @@ defined('MOODLE_INTERNAL') || die();
 if ($ADMIN->fulltree) {
 
     // Boost provides a nice setting page which splits settings onto separate tabs. We want to use it here.
-    $settings = new theme_boost_admin_settingspage_tabs('themesettingphoto', get_string('configtitle', 'theme_photo'));
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingphoto', get_string('configtitle', 'theme_ikbfu2021'));
 
     // Each page is a tab - the first is the "General" tab.
-    $page = new admin_settingpage('theme_photo_general', get_string('generalsettings', 'theme_photo'));
+    $page = new admin_settingpage('theme_ikbfu2021_general', get_string('generalsettings', 'theme_ikbfu2021'));
 
     // Replicate the preset setting from boost.
-    $name = 'theme_photo/preset';
-    $title = get_string('preset', 'theme_photo');
-    $description = get_string('preset_desc', 'theme_photo');
+    $name = 'theme_ikbfu2021/preset';
+    $title = get_string('preset', 'theme_ikbfu2021');
+    $description = get_string('preset_desc', 'theme_ikbfu2021');
     $default = 'default.scss';
 
     // We list files in our own file area to add to the drop down. We will provide our own function to
     // load all the presets from the correct paths.
     $context = context_system::instance();
     $fs = get_file_storage();
-    $files = $fs->get_area_files($context->id, 'theme_photo', 'preset', 0, 'itemid, filepath, filename', false);
+    $files = $fs->get_area_files($context->id, 'theme_ikbfu2021', 'preset', 0, 'itemid, filepath, filename', false);
 
     $choices = [];
     foreach ($files as $file) {
@@ -44,9 +44,9 @@ if ($ADMIN->fulltree) {
     $page->add($setting);
 
     // Preset files setting.
-    $name = 'theme_photo/presetfiles';
-    $title = get_string('presetfiles','theme_photo');
-    $description = get_string('presetfiles_desc', 'theme_photo');
+    $name = 'theme_ikbfu2021/presetfiles';
+    $title = get_string('presetfiles','theme_ikbfu2021');
+    $description = get_string('presetfiles_desc', 'theme_ikbfu2021');
 
     $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
         array('maxfiles' => 20, 'accepted_types' => array('.scss')));
@@ -54,9 +54,9 @@ if ($ADMIN->fulltree) {
 
     // Variable $brand-color.
     // We use an empty default value because the default colour should come from the preset.
-    $name = 'theme_photo/brandcolor';
-    $title = get_string('brandcolor', 'theme_photo');
-    $description = get_string('brandcolor_desc', 'theme_photo');
+    $name = 'theme_ikbfu2021/brandcolor';
+    $title = get_string('brandcolor', 'theme_ikbfu2021');
+    $description = get_string('brandcolor_desc', 'theme_ikbfu2021');
     $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
@@ -65,17 +65,17 @@ if ($ADMIN->fulltree) {
     $settings->add($page);
 
     // Advanced settings.
-    $page = new admin_settingpage('theme_photo_advanced', get_string('advancedsettings', 'theme_photo'));
+    $page = new admin_settingpage('theme_ikbfu2021_advanced', get_string('advancedsettings', 'theme_ikbfu2021'));
 
     // Raw SCSS to include before the content.
-    $setting = new admin_setting_configtextarea('theme_photo/scsspre',
-        get_string('rawscsspre', 'theme_photo'), get_string('rawscsspre_desc', 'theme_photo'), '', PARAM_RAW);
+    $setting = new admin_setting_configtextarea('theme_ikbfu2021/scsspre',
+        get_string('rawscsspre', 'theme_ikbfu2021'), get_string('rawscsspre_desc', 'theme_ikbfu2021'), '', PARAM_RAW);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
     // Raw SCSS to include after the content.
-    $setting = new admin_setting_configtextarea('theme_photo/scss', get_string('rawscss', 'theme_photo'),
-        get_string('rawscss_desc', 'theme_photo'), '', PARAM_RAW);
+    $setting = new admin_setting_configtextarea('theme_ikbfu2021/scss', get_string('rawscss', 'theme_ikbfu2021'),
+        get_string('rawscss_desc', 'theme_ikbfu2021'), '', PARAM_RAW);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
