7326203720a826db2a5933c8cfbd25884e1d37c5
Gleb Lobanov <gleblobanov@users.noreply.github.com>
2021-01-28 16:31:48 +0200

    Add configs.

---
+++ lang/ru/theme_ikbfu.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+* @package   theme_ikbfu
+* @copyright 2021 Gleb Lobanov
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'Тема Балтийского федерального университета им. И. Канта.';
+$string['pluginname'] = 'IKBFU';
---
+++ lib.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+* @package   theme_ikbfu
+* @copyright 2021 Gleb Lobanov
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
