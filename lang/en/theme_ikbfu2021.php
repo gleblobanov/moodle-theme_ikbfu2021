d8e9b10c4184785b053f8880c8a3421cc2e61ffe
Gleb Lobanov <gleblobanov@users.noreply.github.com>
2021-01-28 16:54:53 +0200

    Add English language files with a name of the plugin.

---
+++ lang/en/theme_ikbfu2021.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+* @package   theme_ikbfu2021
+* @copyright 2021 Gleb Lobanov
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'Immanuel Kant Baltic Federal University Theme';
+$string['pluginname'] = 'IKBFU2021';
