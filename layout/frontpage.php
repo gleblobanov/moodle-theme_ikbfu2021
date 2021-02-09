204db161eff2638b83fdabe4bd1830a38928873e
Gleb Lobanov <gleblobanov@users.noreply.github.com>
2021-02-09 10:24:32 +0200

    Remove a content section from the frontpage and leave only blocks.

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -13,11 +13,12 @@ defined('MOODLE_INTERNAL') || die();
 user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
 require_once($CFG->libdir . '/behat/lib.php');
 
-if (isloggedin()) {
-    $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
-} else {
-    $navdraweropen = false;
-}
+// if (isloggedin()) {
+//     $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
+// } else {
+//     $navdraweropen = false;
+// }
+$navdraweropen = false;
 $extraclasses = [];
 if ($navdraweropen) {
     $extraclasses[] = 'drawer-open-left';
